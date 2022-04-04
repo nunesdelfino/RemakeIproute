b7796ca66404cfa9ff9c04ef2c1109945a9cce70
nunesdelfino <gabrielnunesdelfinoo@outlook.com>
2022-04-04 12:22:22 -0300

    versao2.1

--- rmkip.py
+++ rmkip.py
@@ -1,12 +1,13 @@
 #!/bin/python
+# Importa os processos necessário
+import subprocess
 
 PlacasDeRede = list() # Para obter placas de rede para obter mais infromações
 Informacoes = list() # Obtem IP e concatena com o nome da placa
 EnderecoMAC = list() # Obterm o endereço MAC
-IpInfo = list() # Lista final de informações
+PlacasComConexao = list()
 
-# Importa os processos necessário
-import subprocess
+
 
 # Executa comando para obter informações de ip usando utilitátio do IpRoute
 # Filtra por : e troca tudo depois de ': ' por '\\n' e depois deleta tudo que está depois de '<' depois substitui ':' por '==' 
@@ -14,6 +15,8 @@ RetornoIpAll = subprocess.Popen("ip a | grep ': ' | sed 's/: /\\n /' | sed 's/ <
 RetornoIpAll = RetornoIpAll.communicate()
 # Transforma a saída do comando em um lista dividida por ' '
 RetornoIpAll = str(RetornoIpAll).split(' ')
+
+print(RetornoIpAll)
 
 # Obtem o nome de cada adptador de rede conectado, Up ou Down
 # Percorre toda a lista gerada anteriormente
@@ -39,21 +42,29 @@ for Placa in PlacasDeRede:
         if(Valor == "inet" and Posicao < len(RetornoShowPlaca)):
             # Cria uma lista concatenando o nome do adaptador e o IP
             Informacoes.append("Nome do Adaptador: " + Placa + (' ' * (10-len(Placa))) + " IPv4: " + RetornoShowPlaca[Posicao+1] + (' ' * (19-len(RetornoShowPlaca[Posicao+1]))))
-        
+            PlacasComConexao.append(Placa)
         # Executa a condição para obter os MACs dos adaptadores de rede
         if(Valor.startswith("link/") and Posicao < len(RetornoShowPlaca)):
             # Cria uma lista com os endereços MAC
             EnderecoMAC.append(RetornoShowPlaca[Posicao+1])
 
-print("Todas os adaptadores de rede")
+print("Todos os adaptadores de rede")
 # Mostra os nomes dos adaptadore de rede unindo com '==' e depois concatenando um '\n'
-print(" == ".join(PlacasDeRede)+ "\n")
+print(" --- ".join(PlacasDeRede)+ "\n")
 
-# Percorre a lista de informações geradas anteriormente
-for x in range(len(Informacoes)):
-    # Mostra o Nome - IP e MAC de cada adaptador
-    print(Informacoes[x] + " Endereço MAC: " + EnderecoMAC[x])
+if(len(PlacasDeRede) > 0):
+    print("\nPlacas com conexão")
+    # Percorre a lista de informações geradas anteriormente
+    for x in range(len(Informacoes)):
+        # Mostra o Nome - IP e MAC de cada adaptador
+        print(Informacoes[x] + " Endereço MAC: " + EnderecoMAC[x])
 
+for item in PlacasComConexao:
+    for item2 in PlacasDeRede:
+        if(item == item2):
+            PlacasDeRede.remove(item)
 
-
-
+if(len(PlacasDeRede) > 0):
+    print("\nPlacas sem conexão")
+    for i in PlacasDeRede:
+        print(i)
